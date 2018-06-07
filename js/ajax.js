90d803959bbe366a4ea614b5d43cb29507d605c4
Naguchi <lucrerakuri@gmail.com>
2018-06-08 02:22:28 +0900

    カクテルの非同期表示

--- js/ajax.js
+++ js/ajax.js
@@ -1,56 +1,63 @@
-$(function(){
+$(function() {
     $(document).ready(function(){
-
-        $.ajax({
-            url:'http://localhost:8000/api/v1/tag_category',
-            type:'GET',
-            data:{
-            }
-        })
-        .done((data) => {
-            $.each(data, function(index, tag_category) {
-                $(`
-                    <li id="tag_category_id_${tag_category['id']}">
-                      <a href="#" class="tit">${tag_category['name']}</a>
-                      <ul id="tag_category_id_${tag_category['id']}_tags"></ul>
-                    </li>
-                `).appendTo("#dropmenu");
-
-                $.each(tag_category['tags'], function(index, tag) {
-                    $(`
-                        <label>
-                          <li>
-                            <input type="checkbox" value="${tag['id']}">${tag['name']}
-                          </li>
-                        </label>
-                    `).appendTo(`#tag_category_id_${tag_category['id']}_tags`);
-                });
-            });
-        })
-        .fail( (data) => {
-        })
-        .always( (data) => {
-        });
+        init();
     });
 
     $('#fetch').on('click',function(){
-//     });
-//     $('#tag_list').change(function(){
-
-        promise
-            .then(getCocktailList)
-            .then(proposalCocktail);
+        addSearchTagName();
+        getCocktailList()
+            .then(function(cocktail_list) {
+                return proposalCocktail(cocktail_list);
+            });
     });
 
-    function addTag() {
-        let tag_list = String($('#tag_list').val());
-        $('#tag_list').val(tag_list + '名前' + '\n');
-    }
+});
 
-    function getCocktailList() {
-console.log("getCocktailList");
-        let proposal_cocktail_number = 0;
+function init() {
+    loadTagCategory();
+}
 
+function loadTagCategory() {
+    $.ajax({
+        url:'http://localhost:8000/api/v1/tag_category',
+        type:'GET',
+        data:{
+        }
+    })
+    .done((data) => {
+        $.each(data, function(index, tag_category) {
+            $(`
+                <li id="tag_category_id_${tag_category['id']}">
+                    <a href="#" class="tit">${tag_category['name']}</a>
+                    <ul id="tag_category_id_${tag_category['id']}_tags"></ul>
+                </li>
+            `).appendTo("#dropmenu");
+
+            $.each(tag_category['tags'], function(index, tag) {
+                $(`
+                    <label>
+                        <li>
+                        <input type="checkbox" value="${tag['id']}">${tag['name']}
+                        </li>
+                    </label>
+                `).appendTo(`#tag_category_id_${tag_category['id']}_tags`);
+            });
+        });
+    })
+    .fail( (data) => {
+    })
+    .always( (data) => {
+    });
+}
+
+function addSearchTagName(tag) {
+    tag_name = 'タグ名';
+    let tag_list = String($('#tag_list').val());
+    $('#tag_list').val(tag_list + tag_name + '\n');
+}
+
+function getCocktailList(tags) {
+    return new Promise(function(resolve, reject) {
         $.ajax({
             url:'http://localhost:8000/api/v1/cocktail',
             type:'GET',
@@ -58,30 +65,18 @@ console.log("getCocktailList");
             }
         })
         .done((data) => {
-            return data;
+            resolve(data);
         })
         .fail( (data) => {
-        })
-        .always( (data) => {
+            reject('error');
         });
-
-    }
-});
-
-function proposalCocktail(cocktail_list) {
-console.log("proposalCocktail");
-        if (!!cocktail_list) {
-            let cocktail_name = cocktail_list[proposal_cocktail_number]['name'];
-            $('#proposal_cocktail_name').text(cocktail_name);
-        }
+    });
 }
 
-var promise = new Promise(
-    function(resolve, reject) {
-        setTimeout(
-            function() {
-                resolve({"message": "success"});
-            }, 3000
-        );
+function proposalCocktail(cocktail_list) {
+    let proposal_cocktail_number = 0;
+    if (!!cocktail_list) {
+        let cocktail_name = cocktail_list[proposal_cocktail_number]['name'];
+        $('#proposal_cocktail_name').text(cocktail_name);
     }
-);
+}
