07862b8946a3876ef988ea602daeec323cdbd817
Naguchi <lucrerakuri@gmail.com>
2018-06-13 03:17:44 +0900

    バックグラウンドからデータ取得できるようにした

--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,4 @@
+var tag_list = [];
 $(function() {
     $(document).ready(function(){
         init();
@@ -10,7 +11,6 @@ $(function() {
                 return proposalCocktail(cocktail_list);
             });
     });
-
 });
 
 function init() {
@@ -18,48 +18,54 @@ function init() {
 }
 
 function loadTagCategory() {
-    $.ajax({
-        url:'http://localhost:8000/api/v1/tag_category',
-        type:'GET',
-        data:{
-        }
-    })
-    .done((data) => {
-        $.each(data, function(index, tag_category) {
-            $(`
-                <li id="tag_category_id_${tag_category['id']}">
-                    <a href="#" class="tit">${tag_category['name']}</a>
-                    <ul id="tag_category_id_${tag_category['id']}_tags"></ul>
-                </li>
-            `).appendTo("#dropmenu");
-
-            $.each(tag_category['tags'], function(index, tag) {
+    fetchTagCategoryList()
+        .then((tag_category_list) => {
+            $.each(tag_category_list, (index, tag_category) => {
                 $(`
-                    <label>
-                        <li>
-                        <input type="checkbox" value="${tag['id']}">${tag['name']}
-                        </li>
-                    </label>
-                `).appendTo(`#tag_category_id_${tag_category['id']}_tags`);
+                    <li id="tag_category_id_${tag_category['id']}">
+                        <a href="#" class="tit">${tag_category['name']}</a>
+                        <ul id="tag_category_id_${tag_category['id']}_tags"></ul>
+                    </li>
+                `).appendTo("#dropmenu");
+
+                $.each(tag_category['tags'], (index, tag) => {
+                    $(`
+                        <label id="tag_id_${tag['id']}">
+                            <li>
+                            <input type="checkbox" value="${tag['id']}">${tag['name']}
+                            </li>
+                        </label>
+                    `).appendTo(`#tag_category_id_${tag_category['id']}_tags`);
+                });
             });
         });
-    })
-    .fail( (data) => {
-    })
-    .always( (data) => {
+}
+
+function fetchTagCategoryList() {
+    return new Promise((resolve, reject) => {
+        // バックグラウンドからタグ・カテゴリを取得
+        $.ajax({
+            url:'https://cocktailwaiter-official.appspot.com/tag-group',
+            type:'GET',
+            data:{
+            }
+        })
+        .done((tag_category_list) => {
+            resolve(JSON.parse(tag_category_list));
+        });
     });
 }
 
 function addSearchTagName(tag) {
     tag_name = 'タグ名';
-    let tag_list = String($('#tag_list').val());
+    tag_list = String($('#tag_list').val());
     $('#tag_list').val(tag_list + tag_name + '\n');
 }
 
 function getCocktailList(tags) {
     return new Promise(function(resolve, reject) {
         $.ajax({
-            url:'http://localhost:8000/api/v1/cocktail',
+            url:'https://cocktailwaiter-official.appspot.com/cocktail',
             type:'GET',
             data:{
             }
@@ -67,7 +73,7 @@ function getCocktailList(tags) {
         .done((data) => {
             resolve(data);
         })
-        .fail( (data) => {
+        .fail((data) => {
             reject('error');
         });
     });
